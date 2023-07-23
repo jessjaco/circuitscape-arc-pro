51a855643043ddd532d2f95d93f345a12e64e6a8
Jesse Anderson <jesse.anderson@gmail.com>
2023-07-23 10:09:05 -0700

    changed julia version

--- src/runner.py
+++ src/runner.py
@@ -6,7 +6,7 @@ def run_circuitscape(config_file: Path, messages) -> None:
     working_dir = Path(__file__).resolve().parent
     wrking_dir = str(working_dir).replace("\\", "/")
     cfg_file = str(config_file).replace("\\", "/")
-    julia_exe = working_dir / "julia-1.8.5-win64/julia-1.8.5/bin/julia.exe"
+    julia_exe = working_dir / "julia-1.9.2/bin/julia.exe"
     julia_script = f'"using Pkg; Pkg.activate(\\"{wrking_dir}\\"); Pkg.instantiate(); using Circuitscape; compute(\\"{cfg_file}\\")"'
     full_command = f"{julia_exe} -e {julia_script}"
     messages.addMessage(full_command)
